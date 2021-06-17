ac918dca3c76fbbab82764e138bde972a16d68bf
ilanb <ilan>
2021-06-17 22:27:34 +0300

    test with default values

--- tests/test_attrsargparser.py
+++ tests/test_attrsargparser.py
@@ -1,13 +1,30 @@
 import attr
+import pytest
 
 from attrsargparser.attrsargparser import AttrsArgparser
 
 
-def test_first_sample_no_default_means_positional_argument():
-    @attr.s(auto_attribs=True)
-    class SampleParser(AttrsArgparser):
-        name: str
+@attr.s(auto_attribs=True)
+class SampleParserStrAttNoDefaultValue(AttrsArgparser):
+    name: str
 
-    args: SampleParser = SampleParser.getargs(["joe"])
-    assert isinstance(args, SampleParser)
+
+@attr.s(auto_attribs=True)
+class SampleParserStrAttWithDefaultValue(AttrsArgparser):
+    name: str
+
+
+def test_first_sample_no_default_means_positional_argument_good():
+    args: SampleParserStrAttNoDefaultValue.getargs(["joe"])
+    assert isinstance(args, SampleParserStrAttNoDefaultValue)
+    assert args.name == "joe"
+
+
+def test_first_sample_no_default_means_positional_argument_bad():
+    with pytest.raises(SystemExit):
+        SampleParserStrAttNoDefaultValue.getargs(["--name", "joe"])
+
+
+def test_with_default_means_kw_argument():
+    args: SampleParserStrAttWithDefaultValue.getargs(["--name", "joe"])
     assert args.name == "joe"
