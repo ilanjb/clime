99f7986d5d274cc676aa2ee6c68254a71f8d9493
ilanb <ilan>
2021-06-17 22:31:13 +0300

    more tests

--- tests/test_attrsargparser.py
+++ tests/test_attrsargparser.py
@@ -9,13 +9,8 @@ class SampleParserStrAttNoDefaultValue(AttrsArgparser):
     name: str
 
 
-@attr.s(auto_attribs=True)
-class SampleParserStrAttWithDefaultValue(AttrsArgparser):
-    name: str
-
-
 def test_first_sample_no_default_means_positional_argument_good():
-    args: SampleParserStrAttNoDefaultValue.getargs(["joe"])
+    args = SampleParserStrAttNoDefaultValue.getargs(["joe"])
     assert isinstance(args, SampleParserStrAttNoDefaultValue)
     assert args.name == "joe"
 
@@ -25,6 +20,16 @@ def test_first_sample_no_default_means_positional_argument_bad():
         SampleParserStrAttNoDefaultValue.getargs(["--name", "joe"])
 
 
-def test_with_default_means_kw_argument():
-    args: SampleParserStrAttWithDefaultValue.getargs(["--name", "joe"])
+@attr.s(auto_attribs=True)
+class SampleParserStrAttWithDefaultValue(AttrsArgparser):
+    name: str = None
+
+
+def test_with_default_means_kw_argument_arg_given():
+    args = SampleParserStrAttWithDefaultValue.getargs(["--name", "joe"])
     assert args.name == "joe"
+
+
+def test_with_default_means_kw_argument_no_arg_given():
+    args = SampleParserStrAttWithDefaultValue.getargs([])
+    assert args.name is None
