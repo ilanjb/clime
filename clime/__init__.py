2a52431bc0a559804ac8ef98533783db7b1cbc29
ilanb <ilan>
2021-06-26 14:15:21 +0300

    package with flit

--- clime/__init__.py
+++ clime/__init__.py
@@ -1,5 +1,7 @@
 """
-The only imports you should need from clime
+clis from attrs classes
 """
+__version__ = '0.0.1'
+
 from clime.parseonce_field_transformer import clime_field_transformer
 from clime.clime import clime
